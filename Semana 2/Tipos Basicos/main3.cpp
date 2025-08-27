ca75cb992709f5da0ca2de9739cca828d7736331
Alumno B303 <Alumno-B303@UCSP.AULAB>
2025-08-27 07:58:24 -0500

    Main

---
+++ Semana 2/Tipos Basicos/main3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    unsigned short val1 = 65534;
+    cout << "El valor es " << val1 << endl;
+    val1 = val1 + 1;
+    cout << "El valor es " << val1 << endl;
+    val1 = val1 + 1;
+    cout << "El valor es " << val1 << endl;
+
+    return 0;
+
+}
